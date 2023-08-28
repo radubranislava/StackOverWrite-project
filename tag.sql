90867c8425d6dcc79371b2f2ffd467a46a3dc1eb
radubranislava <141508369+radubranislava@users.noreply.github.com>
2023-08-28 21:36:52 +0200

    Update tag.sql

--- tag.sql
+++ tag.sql
@@ -1,48 +1,10 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Aug 23, 2023 at 11:50 PM
--- Server version: 10.4.28-MariaDB
--- PHP Version: 8.2.4
+CREATE TABLE tag (
+  id_taga int identity(1,1) NOT NULL primary key,
+  naziv_taga varchar(50) NOT NULL
+) 
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
+INSERT INTO tag (naziv_taga)
+VALUES ('rekurzija');
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `stackoverwrite`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `tag`
---
-
-CREATE TABLE `tag` (
-  `id_taga` int(11) NOT NULL,
-  `naziv_taga` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `tag`
---
-ALTER TABLE `tag`
-  ADD PRIMARY KEY (`id_taga`);
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+INSERT INTO tag (naziv_taga)
+VALUES ('tipizacija');
