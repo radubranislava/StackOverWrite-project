047efeab5f9183d4d1e2f19b6c9c06484260a9ca
radubranislava <141508369+radubranislava@users.noreply.github.com>
2023-08-30 06:56:25 +0200

    Update korisnik.sql

--- korisnik.sql
+++ korisnik.sql
@@ -1,13 +1,15 @@
-create table korisnik
-(
-    id_korisnika int IDENTITY(1,1) not null primary key, 
-    ime varchar(50) not null, 
-    prezime varchar(50) not null,
-    email varchar(50) not null, 
-    lozinka varchar(50) not null
-)
-INSERT INTO korisnik (ime,prezime,email,lozinka)
-VALUES ('Milica','Ilijev','milicailijev@gmail.com','!milica123!');
+CREATE DATABASE stackoverwrite
 
-INSERT INTO korisnik (ime,prezime,email,lozinka)
-VALUES ('Milan','Markovic','milianmarkovc@gmail.com','!milan!123');
+CREATE TABLE korisnik (
+id_korisnika int IDENTITY (1,1) NOT NULL PRIMARY KEY,
+email varchar(30) NOT NULL,
+username varchar(30) NOT NULL,  
+lozinka varchar(30) NOT NULL
+);
+
+
+INSERT INTO korisnik (email,username,lozinka)
+VALUES ('milicailijev@gmail.com','Milica Ilijev','!milica123!');
+
+INSERT INTO korisnik (email,username,lozinka)
+VALUES ('milianmarkovic@gmail.com','Milan Markovic ','!milan!123');
