8dac7e615c8b2d9a6566c71ec6eba5607005ffa5
radubranislava <141508369+radubranislava@users.noreply.github.com>
2023-08-30 06:45:25 +0200

    Update pitanjetag.sql

--- pitanjetag.sql
+++ pitanjetag.sql
@@ -1,19 +1,24 @@
+
 CREATE TABLE pitanjetag (
-  id_pitanja_taga int identity(1,1) NOT NULL primary key,
-  id_pitanja int  NOT NULL,
-  id_taga int  NOT NULL
-) 
+  id_pitanja_taga int IDENTITY (1,1) NOT NULL PRIMARY KEY,
+  id_pitanja int NOT NULL,
+  id_taga int NOT NULL
+);
 
 
-alter table pitanjetag
-add constraint fkpitanjetagtag foreign key (id_taga)
+ALTER TABLE pitanjetag
+ADD CONSTRAINT fkpitanjetagtag FOREIGN KEY (id_taga)
 REFERENCES tag(id_taga)
+ON DELETE NO ACTION
+ON UPDATE CASCADE;
 
-alter table pitanjetag
-add constraint fkpitanjetagpitanje foreign key (id_pitanja)
+ALTER TABLE  pitanjetag
+ADD CONSTRAINT fkpitanjetagpitanje FOREIGN KEY (id_pitanja)
 REFERENCES pitanje(id_pitanja)
+ON DELETE NO ACTION
+ON UPDATE CASCADE;
+
 INSERT INTO pitanjetag (id_pitanja,id_taga)
 VALUES (1,1);
 INSERT INTO pitanjetag (id_pitanja,id_taga)
 VALUES (2,2);
-
