3969f6c54b9808e302c6a90e95679fb7653f4cfe
radubranislava <141508369+radubranislava@users.noreply.github.com>
2023-08-30 12:36:54 +0200

    Update pitanje.sql

--- pitanje.sql
+++ pitanje.sql
@@ -1,20 +1,12 @@
 CREATE TABLE pitanje (
-  id_pitanja int identity(1,1) NOT NULL primary key,
+  id_pitanja int IDENTITY (1,1) NOT NULL PRIMARY KEY,
   naslov text NOT NULL,
   sadrzaj text NOT NULL,
   datum_postavljanja datetime NOT NULL,
- id_korisnika int NOT NULL
-) 
+);
 
-alter table pitanje
-add constraint fkpitanjekorisnik foreign key (id_korisnika)
-REFERENCES korisnik (id_korisnika)
-on delete no action
-on update cascade;
+INSERT INTO pitanje (naslov,sadrzaj,datum_postavljanja)
+VALUES ('Upotreba rekurzije u programiranju','Kako se rekurzivne funkcije koriste za resavanje problema i pruziti primer jednog problema koji se elegantno resava kroz rekurziju?','2023-08-27 14:30:00');
 
-INSERT INTO pitanje (naslov,sadrzaj,datum_postavljanja,id_korisnika)
-VALUES ('Upotreba rekurzije u programiranju','Kako se rekurzivne funkcije koriste za resavanje problema i pruziti primer jednog problema koji se elegantno resava kroz rekurziju?','2023-08-27 14:30:00',2);
-
-INSERT INTO pitanje (naslov,sadrzaj,datum_postavljanja,id_korisnika)
-VALUES ('Staticka i dinamicka tipizacija','Kako se razlikuju staticka i dinamicka tipizacija u programiranju? Koje su glavne prednosti i mane svakog pristupa? Dajte primer kako bi se lakse razumela njihova razlika i uticaj na pisanje koda.','2023-08-29 19:30:00',1);
-
+INSERT INTO pitanje (naslov,sadrzaj,datum_postavljanja)
+VALUES ('Staticka i dinamicka tipizacija','Kako se razlikuju staticka i dinamicka tipizacija u programiranju? Koje su glavne prednosti i mane svakog pristupa? Dajte primer kako bi se lakse razumela njihova razlika i uticaj na pisanje koda.','2023-08-29 19:30:00');
