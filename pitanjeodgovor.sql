8b222bac9b3d7d64ad38a4563510ffa490d05c04
radubranislava <141508369+radubranislava@users.noreply.github.com>
2023-08-30 06:43:16 +0200

    Update pitanjeodgovor.sql

--- pitanjeodgovor.sql
+++ pitanjeodgovor.sql
@@ -1,21 +1,23 @@
-CREATE TABLE pitanjeodgovor(
-  id_pitanja_odgovora int identity(1,1) NOT NULL primary key,
+
+CREATE TABLE pitanjeodgovor (
+  id_pitanja_odgovora int IDENTITY (1,1) NOT NULL PRIMARY KEY,
   id_odgovora int NOT NULL,
   id_pitanja int NOT NULL
-)
-
-alter table pitanjeodgovor
-add constraint fkpitanjeodgovorodgovor foreign key (id_pitanja)
+);
+ALTER TABLE pitanjeodgovor
+ADD CONSTRAINT fkpitanjeodgovorodgovor FOREIGN KEY (id_pitanja)
 REFERENCES pitanje(id_pitanja)
+ON DELETE NO ACTION
+ON UPDATE CASCADE;
 
-
-alter table pitanjeodgovor
-add constraint fkpitanjeodgovorpitanje foreign key (id_odgovora)
+ALTER TABLE pitanjeodgovor
+ADD CONSTRAINT fkpitanjeodgovorpitanje FOREIGN KEY (id_odgovora)
 REFERENCES odgovor(id_odgovora)
+ON DELETE NO ACTION
+ON UPDATE CASCADE;
 
 INSERT INTO pitanjeodgovor(id_odgovora,id_pitanja)
 VALUES (1,1)
 
 INSERT INTO pitanjeodgovor(id_odgovora,id_pitanja)
 VALUES (2,2)
-
