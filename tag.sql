c95e3f6decc795d5548404b7bbe8801d76b2d4f6
radubranislava <141508369+radubranislava@users.noreply.github.com>
2023-08-30 12:38:58 +0200

    Update tag.sql

--- tag.sql
+++ tag.sql
@@ -1,10 +1,20 @@
 CREATE TABLE tag (
-  id_taga int identity(1,1) NOT NULL primary key,
+  id_taga int IDENTITY (1,1) NOT NULL PRIMARY KEY,
   naziv_taga varchar(50) NOT NULL
-) 
+);
+
 
 INSERT INTO tag (naziv_taga)
 VALUES ('rekurzija');
+
+INSERT INTO tag (naziv_taga)
+VALUES ('for petlja');
+
+INSERT INTO tag (naziv_taga)
+VALUES ('petlja');
+
+INSERT INTO tag (naziv_taga)
+VALUES ('ispis brojeva');
 
 INSERT INTO tag (naziv_taga)
 VALUES ('tipizacija');
