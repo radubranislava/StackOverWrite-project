0678efc9a56722aa12a34230030ec2c4abc511e9
radubranislava <141508369+radubranislava@users.noreply.github.com>
2023-08-30 12:39:52 +0200

    Update korisnik.sql

--- korisnik.sql
+++ korisnik.sql
@@ -3,14 +3,8 @@ CREATE TABLE korisnik (
 id_korisnika int IDENTITY (1,1) NOT NULL PRIMARY KEY,
 email varchar(30) NOT NULL,
 username varchar(30) NOT NULL,  
-
 lozinka varchar(30) NOT NULL
 );
-
-
-lozinka varchar(30) NOT NULL 
-);
-
 
 INSERT INTO korisnik (email,username,lozinka)
 VALUES ('milicailijev@gmail.com','Milica Ilijev','!milica123!');
